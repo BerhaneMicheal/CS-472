3df69b60416c4140120729689a702be392115e6d
Berhane <berhane.gebremicael@miu.edu>
2023-05-17 09:14:01 -0400

    Update

---
+++ NodeFolder/Tuesday's assignment/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/calculate', (req, res) => {
+    const num1 = parseFloat(req.body.num1);
+    const num2 = parseFloat(req.body.num2);
+    const operation = req.body.operation;
+    let result;
+
+    switch(operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num1 / num2;
+            break;
+        default:
+            res.status(400).send('Invalid operation');
+            return;
+    }
+
+    res.send(`The answer is: ${result} <br/> <a href="/">Another calculation</a>`);
+});
+
+app.listen(port, () => {
+    console.log(`Calculator app listening at http://localhost:${port}`);
+});
