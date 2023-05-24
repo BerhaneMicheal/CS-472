ef6a99767dd1e7a59bfdc34031c58ad8a868344d
Berhane <berhane.gebremicael@miu.edu>
2023-05-24 10:43:08 -0400

    Update

--- onlinedictionary/public/dict.js
+++ onlinedictionary/public/dict.js
@@ -1,23 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const searchBtn = document.querySelector("#search-button");
-  const inputBox = document.querySelector("#input-box");
-  const resultList = document.querySelector("#result");
+$(document).ready(function() {
+    const searchBtn = $("#search-button");
+    const inputBox = $("#input-box");
+    const resultList = $("#result");
 
-  const printData = (data) => {
-      resultList.innerHTML = '';
-      data.forEach(item => {
-          let liElement = document.createElement('li');
-          liElement.innerHTML = `<span class="wordtype">${item.wordtype}</span>::${item.definition}`;
-          resultList.appendChild(liElement);
-      });
-  };
+    const printData = (data) => {
+        resultList.empty();
+        $.each(data, function(i, item) {
+            let liElement = $('<li>').html(`<span class="wordtype">${item.wordtype}</span>::${item.definition}`);
+            resultList.append(liElement);
+        });
+    };
 
-  searchBtn.addEventListener('click', () => {
-      let word = inputBox.value;
-      console.log(word);
-      fetch(`http://localhost:3000/search/${word}`)
-          .then(response => response.json())
-          .then(printData)
-          .catch(console.error);
-  });
+    searchBtn.click(function() {
+        let word = inputBox.val();
+        console.log(word);
+        $.getJSON(`http://localhost:3000/search/${word}`, function(data) {
+            printData(data);
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            console.log("Request Failed: " + err);
+        });
+    });
 });
